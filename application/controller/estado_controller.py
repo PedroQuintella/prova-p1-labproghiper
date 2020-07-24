707b8b77399a1b1fff76a971d59c8ab6aac0c352
Pedro Quintella <pedro.q2000@outlook.com>
2020-07-23 22:20:54 -0300

    Feat: Design da página de estado e criação de suas funcionalidades
    Foi feito o design e criadas as funcionalidades da página 'estado.html'. O Controller da página, 'estado_controller.py', foi configurado para as novas funcionalidades. Foram adicionadas novas estilizações no 'style.css', por conta da nova página.

--- application/controller/estado_controller.py
+++ application/controller/estado_controller.py
@@ -1,7 +1,11 @@
-from flask import render_template
+from flask import render_template, request
 from application import app
+from application.model.entity.estado import Estado
+from application.model.dao.estado_dao import EstadoDAO
 
 
-@app.route("/estado")
-def estado():
-    return render_template("estado.html")
+@app.route("/estado/<estado_id>")
+def estado(estado_id):
+    estado_dao = EstadoDAO()
+    estado = estado_dao.buscar_por_id(estado_id)
+    return render_template("estado.html", estado = estado)
